ed91e383f74aa3c8e3699fa179549e4d44f3de85
farellinov <linovfarel@gmail.com>
2026-09-02 23:22:50 +0700

    belajar asik

---
+++ lagu_natal_1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("🎄✨ MERRY CHRISTMAS ✨🎄")
+	fmt.Println()
+	fmt.Println("        *")
+	fmt.Println("       ***")
+	fmt.Println("      *****")
+	fmt.Println("     *******")
+	fmt.Println("    *********")
+	fmt.Println("   ***********")
+	fmt.Println("  *************")
+	fmt.Println("       |||")
+	fmt.Println("       |||")
+	fmt.Println()
+	fmt.Println("🎁 Semoga Natalmu penuh kebahagiaan! 🎁")
+	fmt.Println("⭐ Have a wonderful Christmas! ⭐")
+}
